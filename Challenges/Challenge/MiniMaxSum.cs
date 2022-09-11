2e3b85fbedaf4f7fd997276cfb67b3abbff3b1e0
gprotta <gprotta@gmail.com>
2022-09-11 10:41:25 -0300

    Update .gitignore and MiniMaxSum.cs

--- Challenges/Challenge/MiniMaxSum.cs
+++ Challenges/Challenge/MiniMaxSum.cs
@@ -5,18 +5,18 @@ namespace Challenge.MiniMaxSum
     {
         public static void miniMaxSum(List<int> arr)
         {
-                List<long> arrTemp = new List<long>();   
-                long sum = 0;  
-                for(int j = 0; j < arr.Count; j++)
-                {
-                    sum += arr.ElementAt(j);
-                }       
-                for(int i =0; i< arr.Count;i++)
-                {  
-                    arrTemp.Add(sum - Convert.ToInt64(arr.ElementAt(i)));
-                }
-                arrTemp.Sort();
-                Console.WriteLine(arrTemp.First() + " " + arrTemp.Last());
+            List<long> arrTemp = new List<long>();   
+            long sum = 0;  
+            for(int j = 0; j < arr.Count; j++)
+            {
+                sum += arr.ElementAt(j);
+            }       
+            for(int i =0; i< arr.Count;i++)
+            {  
+                arrTemp.Add(sum - Convert.ToInt64(arr.ElementAt(i)));
+            }
+            arrTemp.Sort();
+            Console.WriteLine(arrTemp.First() + " " + arrTemp.Last());
         }
     }
 }
